58ced781c87d6c82633df8e46a00a2e60fb805e2
uma shankar <umashankar.kousha@gmail.com>
2025-09-25 20:02:37 +0530

    improved form validation with aleret

--- app.js
+++ app.js
@@ -5,40 +5,54 @@ let calculateTipBtnEl = document.getElementById("calculateTipBtn");
 let resetBtnEl = document.getElementById("resetBtn");
 let formEl = document.getElementById("form");
 let currencyEl = document.getElementById("currency");
-let billAmountContainerEl = document.getElementById("billAmountContainer");
-let tipContainerEl = document.getElementById("tipContainer");
-let numberOfpersonsContainerEl = document.getElementById(
-  "numberOfpersonsContainer"
-);
+
 let tipEl = document.getElementById("tip");
 let totalEl = document.getElementById("total");
 let perPersonEl = document.getElementById("perPerson");
 
-let warningEl = document.createElement("p");
-warningEl.textContent = "required";
-warningEl.classList.add("warning");
-
-billAmountEl.addEventListener("blur", () => {
-  if (billAmountEl.value == "") {
-    billAmountContainerEl.appendChild(warningEl);
-  } else {
-    billAmountContainerEl.removeChild(warningEl);
-  }
-});
-
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
+  let valid = true;
   let billAmount = parseInt(billAmountEl.value);
 
   let tipPercentage = parseInt(tipPercentageEl.value);
   let persons = parseInt(numberOfPersonsEl.value);
-  let tip = Math.round((billAmount / 100) * tipPercentage);
-  let total = tip + billAmount;
 
-  let perPerson = total / persons;
+  if (
+    billAmount == "" ||
+    isNaN(Number(billAmount)) ||
+    Number(billAmount) <= 0
+  ) {
+    alert("Please enter a valid bill amount");
+    valid = false;
+  }
+  if (
+    tipPercentage == "" ||
+    isNaN(Number(tipPercentage)) ||
+    Number(tipPercentage) < 0
+  ) {
+    alert("Please enter a valid Tip Percentage");
 
-  let symbol = currencyEl.value;
-  tipEl.innerHTML = `${symbol} ${tip}`;
-  totalEl.innerHTML = `${symbol} ${Math.round(total)}`;
-  perPersonEl.innerHTML = `${symbol} ${Math.round(perPerson)}`;
+    valid = false;
+  }
+  if (persons == "" || isNaN(Number(persons)) || Number(persons) <= 0) {
+    alert("Please enter a valid number of persons");
+
+    valid = false;
+  }
+  if (valid) {
+    let tip = Math.round((billAmount / 100) * tipPercentage);
+    let total = tip + billAmount;
+
+    let perPerson = total / persons;
+
+    let symbol = currencyEl.value;
+    tipEl.innerHTML = `${symbol} ${tip}`;
+    totalEl.innerHTML = `${symbol} ${Math.round(total)}`;
+    perPersonEl.innerHTML = `${symbol} ${Math.round(perPerson)}`;
+  }
+});
+
+formEl.addEventListener("reset", function (event) {
+  event.preventDefault();
 });
